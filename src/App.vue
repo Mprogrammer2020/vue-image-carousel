4449d01d6bd19e5cd5a0aee3a5ccbfc475bb3825
Gurmukh Singh <mobileexpert1234@gmail.com>
2024-04-09 17:17:48 +0530

    App.vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ImageCarousel :images="imageList" />
+  </div>
+</template>
+
+<script>
+import ImageCarousel from 'vue-carousel';
+
+export default {
+  name: 'App',
+  components: {
+    ImageCarousel,
+  },
+  data() {
+    return {
+      imageList: [
+        'https://example.com/image1.jpg',
+        'https://example.com/image2.jpg',
+        'https://example.com/image3.jpg',
+      ],
+    };
+  },
+};
+</script>
